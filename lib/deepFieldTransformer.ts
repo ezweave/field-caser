c84397c63c78f30f9bde524c8d40ad7a22dea052
Matt Weaver <ezweave@gmail.com>
2023-02-06 11:19:23 -0700

    version bumped and fixed array issue

--- lib/deepFieldTransformer.ts
+++ lib/deepFieldTransformer.ts
@@ -1,27 +1,25 @@
-import { isArray, isObject, map, partialRight, reduce } from 'lodash'
-/**
- * This version of lodash/fp seems to muck up the arguments for reduce.  It does
- * not pass the key.
- *
- * @param identitee
- * @returns {*}
- */
-
-const reduceFP = (func, collector) => partialRight(reduce, func, collector)
+import { isArray, isObject, map, reduce } from 'lodash'
 
 export type StringTransformer = (string) => string
 
-export type DeepFieldTransformer = <T>(stringTransformer: StringTransformer) => (source: unknown) => T
-
-export const deepFieldTransformer: DeepFieldTransformer = (stringTransformer) =>
-  reduceFP(
-    (result, value, key) => ({
-      ...result,
-      [stringTransformer(key)]: isArray(value)
-        ? map(value, (entry) => deepFieldTransformer(stringTransformer)(entry))
-        : isObject(value)
-        ? deepFieldTransformer(stringTransformer)(value)
-        : value,
-    }),
-    {}
-  )
+export const deepFieldTransformer = <T extends any>(
+  stringTransformer: StringTransformer
+) => (
+  source: any
+) => {
+    if (isObject(source)) {
+      return reduce(source, (result, value, key) => ({
+        ...result,
+        [stringTransformer(key)]: isArray(value)
+          ? map(value, (entry) => deepFieldTransformer(stringTransformer)(entry))
+          : isObject(value)
+            ? deepFieldTransformer(stringTransformer)(value)
+            : value,
+      }),
+        {})
+    }
+    if (isArray(source)) {
+      return map(source, deepFieldTransformer(stringTransformer))
+    }
+    return source as unknown as T
+  }
