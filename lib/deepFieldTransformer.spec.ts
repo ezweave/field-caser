e17618302fb3893f60b76bdfc6a1fe76cb9e71fd
Matt Weaver <ezweave@gmail.com>
2023-02-06 11:20:59 -0700

    version bumped and fixed array issue (#7)

--- lib/deepFieldTransformer.spec.ts
+++ lib/deepFieldTransformer.spec.ts
@@ -1,9 +1,14 @@
-import { toUpper } from 'lodash'
+import { snakeCase, toUpper } from 'lodash'
 import { deepFieldTransformer } from './deepFieldTransformer'
 import * as jsonWithSnakeCaseFields from './__fixtures/jsonWithSnakeCaseFields.json'
+import arrayExample from './__fixtures/arrayExample.json'
 
 describe('deepFieldTransformer', () => {
   it('converts nested field names to using a converter', () => {
     expect(deepFieldTransformer(toUpper)(jsonWithSnakeCaseFields)).toMatchSnapshot()
+  })
+  it.only('handles arrays', () => {
+    const results = deepFieldTransformer(snakeCase)(arrayExample)
+    expect(results).toMatchSnapshot()
   })
 })
